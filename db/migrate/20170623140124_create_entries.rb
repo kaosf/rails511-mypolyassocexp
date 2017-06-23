cc19d36f975a08e2ec0de210126a2d11065804b8
ka <ka.kaosf@gmail.com>
2017-06-23 23:02:13 +0900

    Add "null: false" limitations to entries columns

--- db/migrate/20170623140124_create_entries.rb
+++ db/migrate/20170623140124_create_entries.rb
@@ -1,8 +1,8 @@
 class CreateEntries < ActiveRecord::Migration[5.1]
   def change
     create_table :entries do |t|
-      t.references :userable, polymorphic: true
-      t.string :title
+      t.references :userable, polymorphic: true, null: false
+      t.string :title, null: false
 
       t.timestamps
     end
