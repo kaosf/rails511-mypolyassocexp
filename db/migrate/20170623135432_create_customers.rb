4ef00f967f8e20bcf0003244787fbd00db0435e6
ka <ka.kaosf@gmail.com>
2017-06-23 22:55:10 +0900

    Add "null: false" limitations to customers columns

--- db/migrate/20170623135432_create_customers.rb
+++ db/migrate/20170623135432_create_customers.rb
@@ -1,8 +1,8 @@
 class CreateCustomers < ActiveRecord::Migration[5.1]
   def change
     create_table :customers do |t|
-      t.string :name
-      t.integer :icon
+      t.string :name, null: false
+      t.integer :icon, null: false
 
       t.timestamps
     end
