e6bd7cc292ef87f9f2cff41e0a52cd55b4221362
ka <ka.kaosf@gmail.com>
2017-06-25 13:29:38 +0900

    Add comments experiment to a.rb and b.rb

--- a.rb
+++ a.rb
@@ -1,12 +1,15 @@
 Customer.destroy_all
 Admin.destroy_all
 Entry.destroy_all
+Comment.destroy_all
 c1 = Customer.create name: 'c1', icon: 1
 a = Admin.create name: 'a1', role: 2
-Entry.create title: 't1 c1', userable: c1
-Entry.create title: 't2 a', userable: a
+e1 = Entry.create title: 't1 c1', userable: c1
+e2 = Entry.create title: 't2 a', userable: a
 c2 = Customer.create name: 'c2', icon: 1
-Entry.create title: 't3 c2', userable: c2
-Entry.create title: 't4 c1', userable: c1
+e3 = Entry.create title: 't3 c2', userable: c2
+e4 = Entry.create title: 't4 c1', userable: c1
 c3 = Customer.create name: 'c3', icon: 1
-Entry.create title: 't5 c3', userable: c3
+e5 = Entry.create title: 't5 c3', userable: c3
+cm1 = Comment.create body: 'cm1 e1 c1', entry: e1, customer: c1
+cm2 = Comment.create body: 'cm2 e1 c2', entry: e1, customer: c2
--- b.rb
+++ b.rb
@@ -2,3 +2,6 @@ Entry.includes(:userable).each { |e| puts e.userable.name }
 Entry.where(userable_type: 'Customer').includes(:userable).each { |e| puts e.userable.icon }
 Entry.where(userable_type: 'Admin').includes(:userable).each { |e| puts e.userable.role }
 # これで定数回オーダーのSQL発行しかされない
+
+Entry.includes(:userable, :comments).each { |e| puts e.userable.name; e.comments.each { |c| puts c.body } }
+Entry.includes(:userable).includes(:comments).each { |e| puts e.userable.name; e.comments.each { |c| puts c.body } }
