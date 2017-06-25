bed8971d38c65a10dadf82810c68fa9a5d069709
ka <ka.kaosf@gmail.com>
2017-06-25 10:36:25 +0900

    Add "null: false" limitations to comments columns

--- db/migrate/20170625013549_create_comments.rb
+++ db/migrate/20170625013549_create_comments.rb
@@ -1,9 +1,9 @@
 class CreateComments < ActiveRecord::Migration[5.1]
   def change
     create_table :comments do |t|
-      t.references :entry, foreign_key: true
-      t.string :body
-      t.references :customer, foreign_key: true
+      t.references :entry, foreign_key: true, null: false
+      t.string :body, null: false
+      t.references :customer, foreign_key: true, null: false
 
       t.timestamps
     end
